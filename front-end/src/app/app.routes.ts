13ab9b51f34df729fda6c54bc4e40c396d6feb39
ahmedehab <ahmedehab3088@gmail.com>
2025-02-16 06:14:26 +0200

    homeroutes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,6 +2,14 @@ import { Routes } from '@angular/router';
 //import { CartComponent } from './Shopping/cart/cart.component';
 import { LoginComponent } from './authentication/login/login.component';  
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
 export const routes: Routes = [
     {path:"", loadChildren:()=>import("./admin-dashboard/admin-dashboard.routes").then(s=>s.adminDashboardRoutes)},
   {
@@ -20,4 +28,12 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
+  {path:"home",component:HomeComponent},
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"catalog",component:CatalogComponent},
+  {path:"catalog",component:CatalogComponent},
+  {path:"catalog/details/:id",component:ProductDetailsComponent},
+  {path:"contact",component:ContactUsComponent},
+  {path:"**",component:NotFoundComponent},
 ];
